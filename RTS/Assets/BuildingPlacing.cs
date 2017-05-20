398234feec8e2d9418a633b4dd7eb9cd2a2129f1
tsantoro92 <tsantoro92@gmail.com>
2017-05-20 15:26:20 -0600

    First commit with Unity project

---
+++ RTS/Assets/BuildingPlacing.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingPlacing : MonoBehaviour
+{
+    static GameObject selectedBuilding;
+    static GameObject previewBuilding;
+
+    public GameObject prefab;
+
+    public void AttachBuildingToCursor(GameObject building)
+    {
+        selectedBuilding = building;
+    }
+
+    Vector3 mouseSpot;
+
+    private void Awake()
+    {
+        previewBuilding = Instantiate(prefab);
+        previewBuilding.SetActive(false);
+        previewBuilding.GetComponent<Collider>().enabled = false;
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            previewBuilding.SetActive(true);
+            previewBuilding.transform.position = hit.point;
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (selectedBuilding != null)
+                    Instantiate(selectedBuilding, hit.point, Quaternion.identity);
+            }
+        }
+        else
+            previewBuilding.SetActive(false);
+    }
+}
