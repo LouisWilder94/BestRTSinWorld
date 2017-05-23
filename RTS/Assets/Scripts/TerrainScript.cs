01b026dd02e5cc68d3b0d8eede1ede75fdd09498
Louis <LouisWilder94@gmail.com>
2017-05-22 18:47:53 -0600

    Added movement for units

---
+++ RTS/Assets/Scripts/TerrainScript.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TerrainScript : MonoBehaviour,IPointerClickHandler {
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Click");
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            SelectionScript.instance.MoveOut(eventData.pointerPressRaycast.worldPosition);
+        }
+    }
+}
