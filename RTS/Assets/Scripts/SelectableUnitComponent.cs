d42feb8087051090c7cecb7c68d135741562fad2
Louis <LouisWilder94@gmail.com>
2017-05-23 14:11:10 -0600

    Got movement/navigation now i just need to implement flocking

--- RTS/Assets/Scripts/SelectableUnitComponent.cs
+++ RTS/Assets/Scripts/SelectableUnitComponent.cs
@@ -3,31 +3,23 @@ using System;
 using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
-
+using UnityEngine.AI;
 
 public class SelectableUnitComponent : MonoBehaviour
 {
     public GameObject selectionCircle;
     public float moveSpeed = 1f;
 
+    public Vector3 target;
+    NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
 
     public void moveTo(Vector3 targetPosition)
     {
-        StartCoroutine(Move(targetPosition));
-    }
-    public IEnumerator Move(Vector3 targetPosition)
-    {
-        float startTime = Time.time;
-        float amount;
-
-        while ((amount = (Time.time - startTime)) < 1)
-        {
-            transform.LookAt(new Vector3(targetPosition.x,transform.position.y,targetPosition.z));
-            transform.Translate(transform.forward * moveSpeed * Time.deltaTime);
-
-            yield return null;
-        }
-
-        
+        agent.SetDestination(targetPosition);
     }
 }
