65e1037911b230a40ff46875204d04220813d7e4
HungryHungrySourceCode <skylor01@gmail.com>
2017-05-26 17:19:45 -0600

    Terrain Shit
    Added a basic hex based terrain generation system. Also added a modifed version of the cam controller from the td project. And added some folders and shit to keep things organized

---
+++ RTS/Assets/Scripts/SceneManagment/SceneFader.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneFader : MonoBehaviour {
+
+    public Image image;
+    public float fadeInRate;
+
+    public AnimationCurve fadeCurve;
+
+    private void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    public void FadeTo(string scene)
+    {
+        StartCoroutine(FadeOut(scene));
+    }
+
+    IEnumerator FadeIn()
+    {
+        float t = 1f;
+        while (t > 0f)
+        {
+            t -= Time.deltaTime * fadeInRate;
+            float a = fadeCurve.Evaluate(t);
+            image.color = new Color(0f, 0f, 0f, t);
+            yield return 0;
+        }
+    }
+
+    IEnumerator FadeOut(string _scene)
+    {
+        float t = 0f;
+        while (t < 1f)
+        {
+            t += Time.deltaTime * fadeInRate;
+            float a = fadeCurve.Evaluate(t);
+            image.color = new Color(0f, 0f, 0f, t);
+            yield return 0;
+        }
+
+        SceneManager.LoadScene(_scene);
+    }
+}
---
+++ RTS/Assets/Scripts/Utility/setLineRendererOrder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setLineRendererOrder : MonoBehaviour {
+
+    public int lineRenderSortingorder;
+
+	// Use this for initialization
+	void Start () {
+        transform.GetComponent<LineRenderer>().sortingOrder = lineRenderSortingorder;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
