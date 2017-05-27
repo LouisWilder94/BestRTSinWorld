64d6034151ff0a40c78827a174af8e8df9536c75
HungryHungrySourceCode <skylor01@gmail.com>
2017-05-26 18:38:40 -0600

    Added Elevation
    Not sure if it updates the mesh collider yet, but, it has elevation shit

--- RTS/Assets/Scripts/Terrain/HexCell.cs
+++ RTS/Assets/Scripts/Terrain/HexCell.cs
@@ -4,7 +4,28 @@ public class HexCell : MonoBehaviour
 {
     public HexCoordinates coordinates;
     public Color color;
+    public RectTransform uiRect;
 
+    public int Elevation
+    {
+        get
+        {
+            return elevation;
+        }
+        set
+        {
+            elevation = value;
+            Vector3 position = transform.localPosition;
+            position.y = value * HexMetrics.elevationStep;
+            transform.localPosition = position;
+
+            Vector3 uiPosition = uiRect.localPosition;
+            uiPosition.z = elevation * -HexMetrics.elevationStep;
+            uiRect.localPosition = uiPosition;
+        }
+    }
+
+    int elevation;
     [SerializeField]
     HexCell[] neighbors;
 
