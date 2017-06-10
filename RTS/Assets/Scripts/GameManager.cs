b728a2d8281602cdfdd504413d7ee064a954732c
HungryHungrySourceCode <skylor01@gmail.com>
2017-06-09 21:58:51 -0600

    Static Terrain + Base player class setup

---
+++ RTS/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameManager : NetworkBehaviour {
+    public static Player[] players;
+    public static GameManager instance;
+
+    public string[] playerTags;
+    //player1 player2 player3 player4
+
+
+    private void Start()
+    {
+        if (instance != null)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public void AddPlayerToList(Player playerToAdd)
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i] = null)
+            {
+                players[i] = playerToAdd;
+                playerToAdd.playerNumber = i;
+                return;
+            }
+        }
+    }
+
+    public Player getPlayer(int playerNumber)
+    {
+        return players[playerNumber];
+    }
+
+}
---
+++ RTS/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+    public Camera playerCamera;
+    public int Money;
+
+    [HideInInspector]
+    public int playerNumber;
+
+    public Vector3 mousePosition
+    {
+        get
+        {
+            Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(inputRay, out hit))
+            {
+                return hit.point;
+            }
+            else
+            {
+                Debug.Log("Mouse Not Hitting Something 8[]D~~~~~");
+                return Vector3.up;
+            }
+        }
+    }
+
+    public Vector3 camPosition
+    {
+        get
+        {
+            return playerCamera.transform.position;
+        }
+        set
+        {
+            playerCamera.transform.position = value;
+        }
+    }
+
+
+
+    private void Awake()
+    {
+        GameManager.instance.AddPlayerToList(this);
+    }
+}
