16476ef37de5b38873c3682e1f7bae2e11b1ea71
Louis <LouisWilder94@gmail.com>
2017-06-11 16:18:11 -0600

    added a gold mine script with a HarvestGold method for

---
+++ RTS/Assets/Scripts/Interactable Objects/GoldMine.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldMine : MonoBehaviour {
+
+    int _gold = 3000;
+    public int gold
+    {
+        get
+        {
+            return _gold;
+        }
+        set
+        {
+            _gold = value;
+        }
+    }
+    // for builders to use
+    public int HarvestGold()
+    {
+        gold -= 5;
+        return 5;
+    }
+	
+}
