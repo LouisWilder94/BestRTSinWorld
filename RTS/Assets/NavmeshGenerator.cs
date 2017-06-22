74b60782edfc8cd97a9c9f6ee905412e8f941e8b
HungryHungrySourceCode <skylor01@gmail.com>
2017-06-21 20:59:22 -0600

    Quick edits
    not much changed

---
+++ RTS/Assets/NavmeshGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavmeshGenerator : MonoBehaviour {
+    public NavMesh mesh;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+    }	
+}
