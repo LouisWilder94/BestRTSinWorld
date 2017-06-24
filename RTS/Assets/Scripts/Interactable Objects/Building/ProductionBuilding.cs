dac419b931a361c7e8d60411273a9a8b4bd3f14a
Tony <tsantoro92@gmail.com>
2017-06-24 10:53:43 -0600

    Tony's last push before demo.

--- RTS/Assets/Scripts/Interactable Objects/Building/ProductionBuilding.cs
+++ RTS/Assets/Scripts/Interactable Objects/Building/ProductionBuilding.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ProductionBuilding : Building {
+    public GameObject spawnPoint;
 
 	void Start () {
-        GameObject spawnPoint = new GameObject();
-	}
+        spawnPoint = new GameObject();
+        spawnPoint.transform.parent = gameObject.transform;
+        spawnPoint.name = "Spawn Point";
+        spawnPoint.transform.position = new Vector3(gameObject.GetComponent<Collider>().bounds.min.x + -0.5f, gameObject.transform.position.y - gameObject.GetComponent<Collider>().bounds.extents.y, gameObject.GetComponent<Collider>().bounds.min.z + -0.5f);
+    }
 }
---
+++ RTS/Assets/UIClickBlocker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIClickBlocker : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public static bool isHovered;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Started hovering");
+        isHovered = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Stopped hovering");
+        isHovered = false;
+    }
+}
